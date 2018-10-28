ff6502107a10eaab28bb849f53135b91a7a78d7f
Distroir <radcraftplay2@gmail.com>
2018-10-28 16:57:25 +0100

    Added basic functionality to "Log Manager" tab
    When BukkitGui is launching, "Log Manager" addon will load informations
    about log files to memory. When activated, "Log Manager" tab will
    now display list of log files stored in "logs" directory under
    server directory. If you select item with filename, that doesn't
    end with ".gz", contents of the file will be displayed in RichTextBox
    bellow SortedListView.

--- bukkitgui2/AddOn/LogManager/LogManagerTab.cs
+++ bukkitgui2/AddOn/LogManager/LogManagerTab.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using MetroFramework.Controls;
 using System.IO;
+using Net.Bertware.Bukkitgui2.Core.FileLocation;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Net.Bertware.Bukkitgui2.AddOn.LogManager
 {
@@ -8,46 +11,123 @@ namespace Net.Bertware.Bukkitgui2.AddOn.LogManager
     {
         public IAddon ParentAddon { get; set; }
 
+        private List<LogFile> _logs;
+
         public LogManagerTab()
         {
             InitializeComponent();
-            //this.Load += LogManagerTab_Load;
+
+            this.Load += LogManagerTab_Load;
+            this.SlvLogs.ItemSelectionChanged += SlvLogs_ItemSelectionChanged;
+
+            LoadLogs();
+        }
+
+        private void SlvLogs_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (e.Item != null && e.IsSelected == true)
+            {
+                LogFile log = e.Item.Tag as LogFile;
+                RtbPreview.Text = log.GetText();
+            }
+            else
+            {
+                RtbPreview.Text = null;
+            }
         }
 
         private void LogManagerTab_Load(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            foreach (LogFile log in _logs)
+            {
+                // Name column
+                ListViewItem item = new ListViewItem();
+                item.Text = log.GetName();
+                item.Tag = log;
+
+                // Date column
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = log.GetDate()
+                });
+
+                SlvLogs.Items.Add(item);
+            }
         }
 
-        public void LoadLogs()
+        private void LoadLogs()
         {
-            throw new NotImplementedException();
+            DirectoryInfo logDir = new DirectoryInfo(Fl.Location(RequestFile.Serverdir) + "\\logs\\");
+            _logs = new List<LogFile>();
+
+            foreach (FileInfo i in logDir.GetFiles())
+            {
+                if (i.Extension != ".log" && i.Extension != ".gz")
+                    continue;
+
+                LogFile f = new LogFile(i);
+                _logs.Add(f);
+            }
         }
     }
 
     internal class LogFile
     {
-        public string Filename { get; private set; }
+        private FileInfo _fileInfo;
 
-        public LogFile(string filename)
+        public LogFile(FileInfo info)
         {
-            Filename = filename;
+            _fileInfo = info;
         }
 
         public string GetName()
         {
-            FileInfo info = new FileInfo(Filename);
-            return info.Name;
+            return _fileInfo.Name;
         }
 
         public string GetDate()
         {
-            throw new NotImplementedException();
+            string date = string.Empty;
+
+            switch (_fileInfo.Extension)
+            {
+                case ".gz":
+                    // Deserialize date from filename
+                    string[] parts = GetName().Split('-');
+                    date = string.Format("{0}-{1}-{2}",
+                        parts[0],
+                        parts[1],
+                        parts[2]);
+                    break;
+                default:
+                    date = string.Format("{0}-{1}-{2}",
+                        _fileInfo.CreationTime.Year,
+                        _fileInfo.CreationTime.Month,
+                        _fileInfo.CreationTime.Day);
+                    break;
+            }
+
+            return date;
         }
-        
+
         public string GetText()
         {
-            throw new NotImplementedException();
+            string text = string.Empty;
+
+            switch (_fileInfo.Extension)
+            {
+                case ".gz":
+                    
+                    break;
+                default:
+                    using (TextReader r = new StreamReader(_fileInfo.FullName))
+                    {
+                        text = r.ReadToEnd();
+                    }
+                    break;
+            }
+
+            return text;
         }
     }
 }
