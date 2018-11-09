3fdaa6cd68051fcb67ce53f07b879043e9ac93a3
Distroir <radcraftplay2@gmail.com>
2018-11-09 15:01:00 +0100

    Fixed invisible splitter in "Log Manager" tab

--- bukkitgui2/AddOn/LogManager/LogManagerTab.Designer.cs
+++ bukkitgui2/AddOn/LogManager/LogManagerTab.Designer.cs
@@ -47,13 +47,13 @@
             this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.splitContainer1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.splitContainer1.Location = new System.Drawing.Point(3, 3);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.splitContainer1.Panel1.Controls.Add(this.SlvLogs);
             this.splitContainer1.Panel1.Controls.Add(this.BtnRefresh);
             // 
